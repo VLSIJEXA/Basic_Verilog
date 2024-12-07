7ac1116a4a64a617795aac5b458659f6de9dcedf
JATIN KATIYAR <katiyarjatin943@gmail.com>
2024-12-07 16:40:51 +0530

    Create ring-counter.v

---
+++ ring-counter.v
@@ -0,0 +1,13 @@
+module exa(clk,int,count);
+input clk,int;
+output reg [7:0]count;
+always@(posedge clk)
+begin
+if(int)
+ count =8'b10000000;
+else begin
+   count <=count+1;
+   count<=count[7];
+end
+    end
+endmodule
